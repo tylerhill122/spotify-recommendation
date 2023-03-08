086a51efa1748676a72cd0bcb58b6808bc2bf657
Tyler Hill <tylergenehill122@gmail.com>
2023-03-08 18:46:27 -0500

    mongoDB capability

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 import main
 
 app = Flask(__name__)
+
+# MongoDB Setup
+app.config["MONGO_URI"] = "mongodb://localhost:27017/spotify"
+mongo = PyMongo(app)
 
 # Spotify global playlist
 playlist_link = 'https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f'
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", data=main.grabber(playlist_link), len = len(main.grabber(playlist_link)['track_name']))
+def index():
+    return render_template("index.html")
+
+@app.route("/playlist")
+def playlist():
+    pList = main.grabber(playlist_link)
+    playlist_db = mongo.db.playlists
+    playlist_db.insert_one(pList)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
